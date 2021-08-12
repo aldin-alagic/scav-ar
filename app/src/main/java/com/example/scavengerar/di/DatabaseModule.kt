6d2297c1888a1a0b22d5ea47edc72cc7755c6e98
Aldin <aldin.alagicc@gmail.com>
2021-08-12 23:57:27 +0200

    Add DatabaseModule dependency container for database dependencies

---
+++ app/src/main/java/com/example/scavengerar/di/DatabaseModule.kt
@@ -0,0 +1,35 @@
+package com.example.scavengerar.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.scavengerar.data.AppDatabase
+import com.example.scavengerar.data.ItemDao
+import com.example.scavengerar.data.LevelDao
+import com.example.scavengerar.utilities.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return AppDatabase.getInstance(context)
+    }
+
+    @Provides
+    fun provideLevelDao(database: AppDatabase): LevelDao {
+        return database.levelDao()
+    }
+
+    @Provides
+    fun provideItemDao(database: AppDatabase): ItemDao {
+        return database.itemDao()
+    }
+}
