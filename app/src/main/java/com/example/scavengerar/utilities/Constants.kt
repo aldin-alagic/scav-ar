c6bb99a96be0f601578c4575db51927fb2b2ef9f
Aldin <aldin.alagicc@gmail.com>
2021-08-12 23:54:19 +0200

    Add file names of JSON files that contain item and level data to Constants

--- app/src/main/java/com/example/scavengerar/utilities/Constants.kt
+++ app/src/main/java/com/example/scavengerar/utilities/Constants.kt
@@ -6,3 +6,6 @@ package com.example.scavengerar.utilities
 const val DATABASE_NAME = "scavenger-db"
 const val API_URL = "localhost:8080/"
 const val S3_URL = "https://scavenger-ar.s3.eu-central-1.amazonaws.com/"
+
+const val LEVELS_DATA_FILENAME = "levels.json"
+const val ITEMS_DATA_FILENAME = "items.json"
