904fa780da014bfdd8e93935f84a488a7eefd7c5
Aldin <aldin.alagicc@gmail.com>
2021-08-12 04:48:06 +0200

    Add constants file to utilities

---
+++ app/src/main/java/com/example/scavengerar/utilities/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.scavengerar.utilities
+
+/**
+ * Constants used throughout the app.
+ */
+const val DATABASE_NAME = "scavenger-db"
+const val API_URL = "localhost:8080/"
+const val S3_URL = "https://scavenger-ar.s3.eu-central-1.amazonaws.com/"
