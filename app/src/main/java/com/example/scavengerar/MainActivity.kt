95ad0f3af3f84193dbb66c1bceb189d7b3797f28
Aldin <aldin.alagicc@gmail.com>
2021-08-13 03:26:36 +0200

    Call MainViewModel in MainActivity to display test data

--- app/src/main/java/com/example/scavengerar/MainActivity.kt
+++ app/src/main/java/com/example/scavengerar/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.scavengerar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.example.scavengerar.data.Level
+import com.example.scavengerar.data.LevelRepository
+import com.example.scavengerar.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 
+/**
+ * Main activity of the application.
+ *
+ * Container for the Buttons & Logs fragments. This activity simply tracks clicks on buttons.
+ */
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        subscribeUi()
+    }
+
+    private fun subscribeUi() {
+        viewModel.items.observe(this, { items ->
+            text.text = items[0].toString()
+        })
     }
 }
