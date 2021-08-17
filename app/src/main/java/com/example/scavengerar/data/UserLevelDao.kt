0aa8535fd3cf4573a820de091380974debbb8e9f
Aldin <aldin.alagicc@gmail.com>
2021-08-17 22:40:32 +0200

    Add UserLevelDao

---
+++ app/src/main/java/com/example/scavengerar/data/UserLevelDao.kt
@@ -0,0 +1,25 @@
+package com.example.scavengerar.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * The Data Access Object for the UserLevel class.
+ */
+@Dao
+interface UserLevelDao {
+    @Query("SELECT * FROM user_level WHERE user_id = :userId")
+    fun getUserLevels(userId: Int): Flow<List<UserLevel>>
+
+    @Query("SELECT * FROM user_level WHERE user_id = :userId AND level_id = :levelId")
+    fun getUserLevel(userId: Int, levelId: Int): Flow<UserLevel>
+
+    @Query("SELECT * FROM user_level WHERE user_id = :userId AND completed = 0")
+    fun getUserActiveLevel(userId: Int): Flow<UserLevel>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(users: List<UserLevel>)
+}
