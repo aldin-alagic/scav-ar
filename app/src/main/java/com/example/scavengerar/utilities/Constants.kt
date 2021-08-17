b291b8d418b9d7e881541c669ba690b23cdee802
Aldin <aldin.alagicc@gmail.com>
2021-08-17 22:41:39 +0200

    Add data files constants to Constants

--- app/src/main/java/com/example/scavengerar/utilities/Constants.kt
+++ app/src/main/java/com/example/scavengerar/utilities/Constants.kt
@@ -9,3 +9,6 @@ const val S3_URL = "https://scavenger-ar.s3.eu-central-1.amazonaws.com/"
 
 const val LEVELS_DATA_FILENAME = "levels.json"
 const val ITEMS_DATA_FILENAME = "items.json"
+const val ROLES_DATA_FILENAME = "roles.json"
+const val USERS_DATA_FILENAME = "users.json"
+const val USER_LEVELS_DATA_FILENAME = "userLevels.json"
