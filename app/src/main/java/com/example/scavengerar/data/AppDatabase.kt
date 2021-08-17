899e934b505757e005d442ef66c2a7290058442c
Aldin <aldin.alagicc@gmail.com>
2021-08-17 22:43:22 +0200

    Add user, role, userLevel and levelItem data classes and DAO's to AppDatabase

--- app/src/main/java/com/example/scavengerar/data/AppDatabase.kt
+++ app/src/main/java/com/example/scavengerar/data/AppDatabase.kt
@@ -7,13 +7,8 @@ import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
-import androidx.work.workDataOf
 import com.example.scavengerar.utilities.DATABASE_NAME
-import com.example.scavengerar.utilities.ITEMS_DATA_FILENAME
-import com.example.scavengerar.utilities.LEVELS_DATA_FILENAME
 import com.example.scavengerar.workers.SeedDatabaseWorker
-import com.example.scavengerar.workers.SeedDatabaseWorker.Companion.LEVELS_FILENAME
-import com.example.scavengerar.workers.SeedDatabaseWorker.Companion.ITEMS_FILENAME
 
 /**
  * Database for storing app data
@@ -31,8 +26,13 @@ import com.example.scavengerar.workers.SeedDatabaseWorker.Companion.ITEMS_FILENA
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun levelDao(): LevelDao
     abstract fun itemDao(): ItemDao
+    abstract fun userDao(): UserDao
+    abstract fun roleDao(): RoleDao
+    abstract fun userLevelDao(): UserLevelDao
+    abstract fun levelItemDao(): LevelItemDao
 
     companion object {
 
@@ -55,14 +55,11 @@ abstract class AppDatabase : RoomDatabase() {
                                override fun onCreate(db: SupportSQLiteDatabase) {
                                    super.onCreate(db)
                                    val request = OneTimeWorkRequestBuilder<SeedDatabaseWorker>()
-                                                 .setInputData(workDataOf(LEVELS_FILENAME to LEVELS_DATA_FILENAME,
-                                                                                ITEMS_FILENAME to ITEMS_DATA_FILENAME))
                                                  .build()
                                    WorkManager.getInstance(context).enqueue(request)
                                }
                            }
                        )
-                       .fallbackToDestructiveMigration()
                        .build()
         }
     }
